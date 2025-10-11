593c6f013dd850c095f3197c83d54860f5eb1201
pavanpai23 <pavanpai2006@gmail.com>
2025-10-11 23:10:25 +0530

    js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,55 @@
- let js='amazing';
-      if(js==='amazing')alert("javascript is for fun");
+//  let js='amazing';
+//       if(js==='amazing')alert("javascript is for fun");
+
+
+//math operator
+// const now=2040;
+
+// const agepav=now-2006;
+// const ageani=now-2016;
+// console.log(agepav,ageani);
+
+
+// console.log(agepav*2, ageani/10,2**3);
+// //2**3 means 2 to the power 3=2*2*2
+
+// const firstname='pavan'
+// const lastname='pai'
+// console.log(firstname+' '+lastname)
+
+// //assignment operator
+// let x = 10 + 5; // x=15
+// x+=10;  //x=x+10=25
+// x*=4;  //x=x*4=100
+// x++;       //x=x+1
+// x--;    //x=x-1
+// x--;
+// console.log(x);
+
+
+// //comparision operator
+// console.log(agepav>ageani);
+
+
+
+// challenge 1
+
+const markweight=78;
+const jhonsweight=92;
+const markheight=1.69;
+const jhonheight=1.95;
+
+bmimark=markweight/markheight**2;
+
+bmijhon=jhonsweight/(jhonheight*jhonheight);
+
+console.log(bmijhon,bmimark);
+
+
+
+if(bmimark>bmijhon){
+    console.log('true');
+}else{
+    console.log('flase');
+    
+}
