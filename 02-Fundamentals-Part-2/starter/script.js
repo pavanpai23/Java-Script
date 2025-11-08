b0d077e5deb661093ea0facfdcb0c8625257e56c
pavanpai23 <pavanpai2006@gmail.com>
2025-11-08 21:27:17 +0530

    JS

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -7,17 +7,30 @@
 //  if(hasdriverslisence) console.log('can drive');
  
 
-function logger(){
-    console.log('my name is pavan');
+// function logger(){
+//     console.log('my name is pavan');
+// }
+
+// logger();
+// logger();
+
+// function fruitprocessor(apples,oranges){    
+//  const juice=`juice with ${apples}apples and ${oranges}oranges`
+//  return juice;
+// }
+
+// const juice=fruitprocessor(5,10);
+// console.log(juice);
+function calcage(birthyear){
+    return 2099-birthyear;
 }
+const age1=calcage(2006);
+console.log(age1);
 
-logger();
-logger();
-
-function fruitprocessor(apples,oranges){    
- const juice=`juice with ${apples}apples and ${oranges}oranges`
- return juice;
+const calcage2=function(birthyear){
+    return 2099-birthyear;
 }
-
-const juice=fruitprocessor(5,10);
-console.log(juice);
+const age2=calcage(2006)
+console.log(age2); //arrow function const calage3=birthyear=>2099-birthyear;
+const age3=calage3(2006);
+console.log(age3);
