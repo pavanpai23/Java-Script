7d4ccada3cc5c34ae246e14c747dc3e96b88adc1
pavanpai23 <pavanpai2006@gmail.com>
2025-11-09 23:02:54 +0530

    JS

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -21,16 +21,40 @@
 
 // const juice=fruitprocessor(5,10);
 // console.log(juice);
-function calcage(birthyear){
-    return 2099-birthyear;
-}
-const age1=calcage(2006);
-console.log(age1);
+// function calcage(birthyear){
+//     return 2099-birthyear;
+// }
+// const age1=calcage(2006);
+// console.log(age1)
 
-const calcage2=function(birthyear){
-    return 2099-birthyear;
+// const calcage2=function(birthyear){
+//     return 2099-birthyear;
+// }
+// const age2=calcage(2006)
+// console.log(age2); //arrow function const calage3=birthyear=>2099-birthyear;
+// const age3=calage3(2006);
+// console.log(age3);
+
+// const yearsleftretirement=(birthyear,name)=>{
+//   const age=2099-birthyear;
+//   const retirement=60-age;
+//   return `${name} retires in ${retirement}years`
+// }
+// console.log(yearsleftretirement(2006,'pavanm'));
+// console.log(yearsleftretirement(2000,'ani'));
+
+
+const fruitcutter=function(fruit){
+  return fruit*5;
 }
-const age2=calcage(2006)
-console.log(age2); //arrow function const calage3=birthyear=>2099-birthyear;
-const age3=calage3(2006);
-console.log(age3);
+      
+const fruitprocessor=function(apples,oranges){ 
+  const applecutter=fruitcutter(apples);
+ const orangescutter=fruitcutter(oranges);
+  const juice=`juice with ${applecutter}apples and ${orangescutter}oranges`;
+ return juice;
+}
+
+console.log(fruitprocessor(2,3));
+
+
