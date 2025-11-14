6d8790848532a10818daf4af4b2707e9f20afd38
pavanpai23 <pavanpai2006@gmail.com>
2025-11-14 23:20:11 +0530

    JS

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -58,25 +58,61 @@
 // console.log(fruitprocessor(2,3));
 
 
-//challange1
-const calcavg=(a,b,c)=>(a+b+c)/3;
-console.log(calcavg(3,4,5));
+// //challange1
+// const calcavg=(a,b,c)=>(a+b+c)/3;
+// console.log(calcavg(3,4,5));
 
-//test1
-const scoreDlopins=calcavg(44,23,71);
-const scorekolas=calcavg(65,54,49);
-console.log(scoreDlopins,scorekolas);
+// //test1
+// const scoreDlopins=calcavg(44,23,71);
+// const scorekolas=calcavg(65,54,49);
+// console.log(scoreDlopins,scorekolas);
 
-const checkwinner=function(avgDlopins,avgkolas){
-  if(avgDlopins>=2*avgkolas){
-    console.log(`dlopins wins (${avgDlopins}) vs ${avgkolas})`);
-  }else if(avgkolas>=2*avgDlopins){
-    console.log(`kolas wins (${avgkolas}) vs ${avgDlopins})`);
-  }
-  else{
-    console.log('no team wins');
-  }
-}
-console.log(scoreDlopins,scorekolas);
+// const checkwinner=function(avgDlopins,avgkolas){
+//   if(avgDlopins>=2*avgkolas){
+//     console.log(`dlopins wins (${avgDlopins}) vs ${avgkolas})`);
+//   }else if(avgkolas>=2*avgDlopins){
+//     console.log(`kolas wins (${avgkolas}) vs ${avgDlopins})`);
+//   }
+//   else{
+//     console.log('no team wins');
+//   }
+// }
+// console.log(scoreDlopins,scorekolas);
 
-checkwinner(576,111 )
+// checkwinner(576,111 )
+
+
+//arrays
+//  const friend1='pavan';
+//  const friend2='ani';
+//  const friend3='ayi';
+
+//  const friend=['pavan','ani','ayi'];
+//  console.log(friend);
+ 
+//  console.log(friend[0]);
+//  console.log(friend[2]);
+
+//  console.log(friend.length);
+ 
+//  console.log(friend[friend.length-1]);
+ 
+const friend= ['pavan','ani','ayi'];
+const newlength=friend.push("bappa");
+console.log(friend);
+console.log(newlength);
+
+friend.unshift('ama');
+console.log(friend);
+
+friend.pop();//last
+console.log(friend);
+
+friend.shift(); //first
+console.log(friend);
+
+console.log(friend.indexOf('ani')); //if there return + or -
+console.log(friend.indexOf('bob'));
+
+console.log(friend.includes('ani'));//if present returns true or flase
+console.log(friend.includes('bob'))
