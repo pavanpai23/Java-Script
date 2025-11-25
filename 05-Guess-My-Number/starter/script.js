42c73bcce631f6e0958366122c51891af3a722cd
pavanpai23 <pavanpai2006@gmail.com>
2025-11-25 22:53:35 +0530

    project_1

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,3 +1,46 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'u won a cup';
+
+// document.querySelector('.number').textContent=12;
+// document.querySelector('.score').textContent=3;
+// console.log(document.querySelector('.guess').value)
+// document.querySelector('.guess').value=23;
+
+
+let score =20;
+
+const secrateNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secrateNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number';
+  } else if (guess === secrateNumber) {
+    document.querySelector('.message').textContent = 'Correct Number';
+  } else if (guess > secrateNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'to high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'you lost the game';
+       document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secrateNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'to high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'you lost the game';
+       document.querySelector('.score').textContent = 0;
+    }
+  }
+});
