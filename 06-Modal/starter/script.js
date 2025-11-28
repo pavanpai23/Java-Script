fcf52c43164bc897e8656e3f7b74274f6165a555
pavanpai23 <pavanpai2006@gmail.com>
2025-11-28 23:14:32 +0530

    JS

---
+++ 06-Modal/starter/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const modal=document.querySelector('.modal');
+const overlay=document.querySelector('.overlay');
+const btnCloseModal=document.querySelector('.close-modal');
+const btnsOpenModal=document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+const CloseModal=function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+const openmodal=function(){
+        console.log("Button is clicked");
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    }
+
+for(let i=0;i<btnsOpenModal.length;i++){
+    btnsOpenModal[i].addEventListener('click',openmodal)
+}
+
+
+
+
+
+btnCloseModal.addEventListener('click',CloseModal);
+overlay.addEventListener('click',CloseModal);
